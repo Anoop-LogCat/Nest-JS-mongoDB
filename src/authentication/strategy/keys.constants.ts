7fd1f7cb5d458d738dc321f87c6db2d43ca841c7
anoop_logcat <71151364+Anoop-LogCat@users.noreply.github.com>
2021-05-09 22:44:25 +0530

    Update keys.constants.ts

--- src/authentication/strategy/keys.constants.ts
+++ src/authentication/strategy/keys.constants.ts
@@ -1,5 +1,5 @@
 export const keys = {
     jwtKey: "top_secret_key_not_share",
-    googleClientID: "642261726172-m3e9cr4b4c5ae7brg2r2574c2palkscs.apps.googleusercontent.com",
-    googleClientSecret: "2OGOLziHwr5Lit_KxOmqhwLl"
+    googleClientID: "Generate a google o-auth api from GCP and provide client ID",
+    googleClientSecret: "Generate a google o-auth api from GCP and provide client Secret"
 }
